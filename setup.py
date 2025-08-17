2c59f895e8af5ea5e66ec2bc714e44f10c473349
mamahoos <m4m4hoos@gmail.com>
2025-08-17 23:55:50 +0330

    chore(version): Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiogram_ask',
-    version='0.1.0', 
+    version='0.1.1', 
     packages=find_packages(),
     install_requires=[
         'aiogram>=3.0.0',
