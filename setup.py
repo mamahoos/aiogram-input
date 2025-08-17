0926b7a0c1dbbc5afad7a40d0988575c90dca30a
mamahoos <m4m4hoos@gmail.com>
2025-08-17 23:04:22 +0330

    chore(setup): Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiogram_ask',
+    version='0.1.0', 
+    packages=find_packages(),
+    install_requires=[
+        'aiogram>=3.0.0',
+    ],
+    author='mamahoos',
+    description='A modular, multi-client library for aiogram to handle user response waiting in Telegram bots.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.8',
+)
