77eacb97f7a4d9a999db2a3380fe3e24d73f15f2
mamahoos <m4m4hoos@gmail.com>
2025-08-17 22:50:49 +0330

    feat(filters): Add PendingUserFilter class to separate filter logic

---
+++ aiogram_ask/filters.py
@@ -0,0 +1,11 @@
+from typing import Dict, Tuple
+from aiogram.types import Message
+from aiogram.filters import BaseFilter
+import asyncio
+
+class PendingUserFilter(BaseFilter):
+    def __init__(self, pending: Dict[Tuple[int, int], 'asyncio.Future[Message]']):
+        self._pending = pending
+
+    async def __call__(self, message: Message) -> bool:
+        return (message.from_user and (message.from_user.id, message.chat.id) in self._pending)
