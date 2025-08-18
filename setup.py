8600a1d0c68de6fadaa4ecaae424ac213b64f01d
mamahoos <m4m4hoos@gmail.com>
2025-08-18 17:03:23 +0330

    docs: update Asker.ask type-hints and bump package version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiogram_ask',
-    version='0.1.3', 
+    version='0.1.4', 
     packages=find_packages(),
     install_requires=[
         'aiogram>=3.0.0',
